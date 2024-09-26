14400de847d091dc22cdb9446306f3d40635fd03
Wenyin Wei <wenyin@mail.ustc.edu.cn>
2024-09-26 21:37:56 +0200

    auto export and exclude `eval` and `include`

--- src/Jynamics.jl
+++ src/Jynamics.jl
@@ -4,8 +4,14 @@ include("Cylind.jl")
 
 using .Cylind
 
+# pick out the functions that are built-in
+builtin_functions = Set([:eval, :include])
+
 for name in names(Cylind, all=true)
-    @eval export $name
+    # only export the functions that are not built-in
+    if !(name in builtin_functions)
+        @eval export $name
+    end
 end
 
 end
