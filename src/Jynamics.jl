6673842a21ecc1d14b5f4fdb2993efb098020cad
Wenyin Wei <wenyin@mail.ustc.edu.cn>
2024-09-26 22:27:06 +0200

    add module File

--- src/Jynamics.jl
+++ src/Jynamics.jl
@@ -1,13 +1,20 @@
 module Jynamics
 
 include("Cylind.jl")
-
+include("File.jl")
 using .Cylind
+using .File
 
 # pick out the functions that are built-in
 builtin_functions = Set([:eval, :include])
 
 for name in names(Cylind, all=true)
+    # only export the functions that are not built-in
+    if !(name in builtin_functions)
+        @eval export $name
+    end
+end
+for name in names(File, all=true)
     # only export the functions that are not built-in
     if !(name in builtin_functions)
         @eval export $name
