34007d3402c8302c1dbf652fe2479d9116a45bee
Wenyin Wei <wenyin@mail.ustc.edu.cn>
2024-09-29 21:30:17 +0200

    add get_tracing_sols dict to store Tracing results so now many codes can be removed

--- src/Jynamics.jl
+++ src/Jynamics.jl
@@ -3,18 +3,21 @@ module Jynamics
 include("Cylind.jl")
 include("File.jl")
 include("Tracing.jl")
+include("PeriodicSearch.jl")
 using .Cylind
 using .File
 using .Tracing
+using .PeriodicSearch
 
 # pick out the functions that are built-in
 builtin_functions = Set([:eval, :include])
 
-for name in names(Cylind, all=true) ∪ names(File, all=true) ∪ names(Tracing, all=true)
+for name in names(Cylind, all=true) ∪ names(File, all=true) ∪ names(Tracing, all=true) 
     # only export the functions that are not built-in
     if !(name in builtin_functions)
         @eval export $name
     end
 end
+export lookfor_cycle
 
 end
