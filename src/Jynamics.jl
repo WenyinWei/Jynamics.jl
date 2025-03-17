724b7efda3099c2c974f25ce98ddabb35751a98b
Wenyin Wei <wenyin@mail.ustc.edu.cn>
2025-03-17 20:41:07 +0100

    Add FourierUtils

--- src/Jynamics.jl
+++ src/Jynamics.jl
@@ -5,16 +5,18 @@ include("Cylind.jl")
 include("File.jl")
 include("Tracing.jl")
 include("PeriodicSearch.jl")
+include("FourierUtils.jl")
 using .DiscreteUtils
 using .Cylind
 using .File
 using .Tracing
 using .PeriodicSearch
+using .FourierUtils
 
 # pick out the functions that are built-in
 builtin_functions = Set([:eval, :include])
 
-for name in names(DiscreteUtils, all=true) ∪ names(Cylind, all=true) ∪ names(File, all=true) ∪ names(Tracing, all=true) 
+for name in names(DiscreteUtils, all=true) ∪ names(Cylind, all=true) ∪ names(File, all=true) ∪ names(Tracing, all=true) ∪ names(FourierUtils, all=true) 
     # only export the functions that are not built-in
     if !(name in builtin_functions)
         @eval export $name
